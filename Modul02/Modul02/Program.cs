f9e93b673c8661de7f32d96c8b6876c052888395
Daniela Naydenova <dani.naydenova@gmail.com>
2016-02-08 18:52:15 +0200

    Модул 02. Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+//			Console.WriteLine ("Hello World!");
+
+			//Примерен коментар на 1 ред
+
+			/* Примерен многоредов коментар
+			 * Ред 1
+			 * Ред 2
+			 * Ред 3
+			 */
+
+			//Дефиниране на променливи
+
+
+			//Събиране на числа
+
+
+			//Писане в конзолата
+
+
+
+
+		}
+	}
+}
